13d373e9d6a174682db272555662c52aedce0303
tallguy188 <cksghd4220@naver.com>
2023-12-13 13:33:29 +0900

    feature: making MemberService
    MemberJoin, findMemberByUsername 추가

--- src/main/java/com/ll/medium/Member/service/MemberService.java
+++ src/main/java/com/ll/medium/Member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.ll.medium.Member.service;
 
 
+import com.ll.medium.Member.entity.Member;
+import com.ll.medium.Member.entity.MemberForm;
 import com.ll.medium.Member.repository.MemberRepository;
+import com.ll.medium.global.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +14,16 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public void joinMember(MemberForm memberForm) {
+        Member member = memberRepository.save(
+                Member.builder()
+                        .username(memberForm.getUsername())
+                        .password(memberForm.getPassword())
+                        .build());
+    }
+
+    public Member findMemberByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found"));
+    }
 }

6fe14c773f0beacff7502df9cc8306e9ae22544d
tallguy188 <cksghd4220@naver.com>
2023-12-13 16:02:03 +0900

    feature: making post entity
    post entity 생성

---
+++ src/main/java/com/ll/medium/post/entity/Post.java
@@ -0,0 +1,32 @@
+package com.ll.medium.post.entity;
+
+
+import com.ll.medium.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@SuperBuilder(toBuilder = true)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private Member member;
+
+}
